16835174a29a80f458a65c8764b4d1e4766378db
Matthew <matthewg.saxsma@gmail.com>
2025-02-18 02:33:32 -0600

    adding testing

--- hopfield_network.py
+++ hopfield_network.py
@@ -8,6 +8,9 @@ def binary_sign(vector):
     vector[vector > 0] = 1
     vector[vector < 0] = -1
     vector[vector == 0] = np.random.choice([-1, 1]) # 50-50 chance for a 0 to be -1 or +1 
+    
+    if np.any(np.isnan(vector)):
+        raise ValueError("Warning: NaN values detected in binary activation vector")
     return vector
 
 # define Hopfield network class
@@ -23,7 +26,7 @@ class HopfieldNetwork:
         self.W = np.array(self.W, dtype=np.float64)
 
     # method for getting network activation after exposed to a pattern
-    def stabilizing_activity(self, start, cycles=40, E=0.15, D=0.15,output=True):
+    def stabilizing_activity(self, start, cycles=40, E=0.15, D=0.15,output=False):
         start = np.array(start, dtype=np.float64)
         for _ in range(cycles):
             activity_change_vector = np.zeros(self.N, dtype=np.float64) # initialize change (delta) vector
@@ -35,7 +38,7 @@ class HopfieldNetwork:
                     activity_change_vector[i] = E * net_input_vector[i] * (1 - start[i]) - D * start[i]
             start = binary_sign(start + activity_change_vector) # update to activation after one cycle
         stable_output_activation = start
-        if np.any(np.isinf(stable_output_activation)) :
+        if np.any(np.isinf(stable_output_activation)):
             print(f"Warning: Inf values detected in activation vector : {stable_output_activation}")
         self.current_activity = stable_output_activation
         if output: return self.current_activity
@@ -54,55 +57,36 @@ class HopfieldNetwork:
             print("Warning: NaN or Inf values detected in updated_W")
         self.W = updated_W
 
-
-N = 40
-# Creating latent profile of caregiving environment
-latent_profile = np.random.choice([-1, 1], size=N)
-
-# Initialize pattern matrix
-pattern_matrix = np.zeros((50, N)) # 50 patterns row, N node columns
-
-# Creating the patterns
-for i in range(np.shape(pattern_matrix)[0]):
-    pattern_matrix[i, :] = np.array(latent_profile * np.random.choice([-1, 1], size=N, p=[0.15, 0.85]))
-
-hopfield1 = HopfieldNetwork(N=N) # create instance
+N = 100
+derivation_correlations = []
+profile_correlations = []
 
 
-for row in range(pattern_matrix.shape[0]):
-    hopfield1.current_activity = binary_sign(hopfield1.current_activity)
-    hopfield1.stabilizing_activity(start=pattern_matrix[row, :]) # let network settle
-    hopfield1.update_weight_matrix(activity_vector=hopfield1.current_activity)
+for simulations in range(10):
+    latent_profile = np.random.choice([-1, 1], size=N)
+    pattern_matrix = np.zeros((50, N)) # 50 patterns row, N node columns
+    # Creating the patterns
+    for i in range(np.shape(pattern_matrix)[0]):
+        pattern_matrix[i, :] = np.array(latent_profile * np.random.choice([-1, 1], size=N, p=[0.3, 0.7]))
 
-for row, _ in enumerate(pattern_matrix):
-    network_response = binary_sign(hopfield1.stabilizing_activity(start=pattern_matrix[row, :],output=True))
-    round(matthews_corrcoef(network_response, pattern_matrix[row,:]),3)
+    hopfield1 = HopfieldNetwork(N=N)
+
+    # training the network
+    for row in range(pattern_matrix.shape[0]):
+        hopfield1.current_activity = binary_sign(hopfield1.current_activity)
+        hopfield1.stabilizing_activity(start=pattern_matrix[row, :]) # let network settle
+        hopfield1.update_weight_matrix(activity_vector=hopfield1.current_activity)
 
 
-network_response = binary_sign(hopfield1.stabilizing_activity(start=latent_profile))
-round(matthews_corrcoef(network_response, latent_profile),3)
+    # testing the network on derivations
+    for row, _ in enumerate(pattern_matrix):
+        network_response = hopfield1.stabilizing_activity(start=pattern_matrix[row, :], output=True)
+        # if network_response is None:
+        #     raise ValueError("NETWORK RESPONSE IS NONE")
+        derivation_correlations.append(round(matthews_corrcoef(network_response, pattern_matrix[row,:]),6))
 
+    # testing the network on latent profiles
+    network_response = binary_sign(hopfield1.stabilizing_activity(start=latent_profile, output=True))
+    profile_correlations.append(round(matthews_corrcoef(network_response, latent_profile),6))
 
-
-# with open("Desktop/testing.txt", "w") as f:
-#     f.write(f"Initial Pattern: \n\n")
-#     np.savetxt(f, hopfield1.current_activity.reshape(1, -1), fmt="%.0f", delimiter=" ")
-#     for row in range(pattern_matrix.shape[0]):
-#         f.write(f"---------------------------------------------------------------------------------------------\n\n")
-#         f.write(f"ITERATION {row + 1}\n\n")
-#         f.write(f"New Pattern: ")
-#         np.savetxt(f, pattern_matrix[row, :].reshape(1, -1), fmt="%.0f", delimiter=" ")
-#         f.write(f"\n\n")
-        
-#         hopfield1.current_activity = binary_sign(hopfield1.current_activity)
-
-#         f.write(f"Activity before exposure to new pattern and settling:\n {hopfield1.current_activity}\n\n")
-#         hopfield1.stabilizing_activity(start=pattern_matrix[row, :]) # let network settle
-#         f.write(f"\nAfter settling, before weight update:\n {hopfield1.current_activity}\n\n")
-
-#         f.write(f"Weights used by pattern {row+1}:\n\n")
-
-#         np.savetxt(f, np.round(hopfield1.W, 7), fmt="%.3f", delimiter=" ")
-        
-#         f.write(f"\n\n")
-#         hopfield1.update_weight_matrix(activity_vector=hopfield1.current_activity)
+np.mean(derivation_correlations), np.mean(profile_correlations)
